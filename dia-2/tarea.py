7df84258ad8735b568064bf45b1098f6dabcd802
Erick Revoredo Jimenez <erick.revoredo58@gmail.com>
2023-05-17 19:35:27 -0500

    tarea

--- dia-2/tarea.py
+++ dia-2/tarea.py
@@ -1,33 +1,44 @@
-n=(4)
+# n=(4)
 
-if n % 2 != 0:
-    print("weird")
-else:
-    if n >=2 and n<=5:
-        print("not weird")
-    elif n>=6 and n <=20:
-        print("weird")
-    elif n>=21:
-        print("not weird")
+# if n % 2 != 0:
+#     print("weird")
+# else:
+#     if n >=2 and n<=5:
+#         print("not weird")
+#     elif n>=6 and n <=20:
+#         print("weird")
+#     elif n>=21:
+#         print("not weird")
 
 
-a=(4)
-b=(3)
+# a=(4)
+# b=(3)
 
-print(int(a/b))
-print(float(a/b))
-print("------------")
-n=(3)
-for numero in range(1,n+1):
-  print(numero,end="")
-print("-------------")
+# print(int(a/b))
+# print(float(a/b))
+# print("------------")
+# n=(3)
+# for numero in range(1,n+1):
+#   print(numero,end="")
+# print("-------------")
 
-n=(6)
-i=()
-for numero in range(0,n):
-    print(numero*numero) 
+# n=(6)
+# i=()
+# for numero in range(0,n):
+#     print(numero*numero) 
     
 
-print("-------------")
-def print_full_name(first_name, last_name):
-    print("Hello " + first_name + " " + last_name + "! You just delved into python.")
+# print("-------------")
+# def print_full_name(first_name, last_name):
+#     print("Hello " + first_name + " " + last_name + "! You just delved into python.")
+
+# print("---------------")
+
+a=(5)
+b=()
+try :
+    print(int((a)/(b)))
+except ZeroDivisionError as e:
+    print("Error Code: ",e)
+except ValueError as a:
+    print ("Error Code: ",a)
