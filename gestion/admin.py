7a29377576aff4a2b57b058a3f41b4ffaac8f454
Kishinkidu <erick.revoredo58@gmail.com>
2023-06-21 23:06:25 -0500

    uso del panel administrativo

---
+++ gestion/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Imagen, Categoria,Producto,Usuario
+
+class ImagenAdmin(admin.ModelAdmin):
+    #clase para agregar algunos filtros al panel adminsitrativo
+    # https://docs.djangoproject.com/en/4.2/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_display
+    list_display=["id","nombre","ubicacion_tag"]
+    #modiffica el ordenamiento predeterminado
+    ordering=["-id"]
+    #sirve para indica que se muestren los campos que no son editables
+    readonly_fields=["id", "ubicacion_tag"]
+    #habilirta un input de busqueda indicando que columnas tiene que buscar
+    # = < que respete mayuscula y minuscula
+    #@ > que haga la busqueda( es como si no le puesiesemos nada)
+    #nada> que no respete mayus ni minus
+    search_fields=["=nombre","ubicacion"]
+
+# para poder visualizar el modelo en el panel administrativo
+admin.site.register(Imagen,ImagenAdmin)
---
+++ gestion/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils.html import mark_safe
+#permissionMixin > modulo de permissions
+#AbstractBaseUser< me sirve para modificar mi auth_user en su totalidad
+
+# Create your models here.
+class Imagen (models.Model):
+        nombre= models.TextField()
+        ubicacion = models.ImageField()
+
+        def __str__(self):
+                #sirvef para indicar como se mostrar la instancia al momento de ser solicitada
+                return self.nombre
+        
+        def ubicacion_tag(self):
+                return mark_safe('<img src="/imagenes/%s" width="150" height="150"/>' %(self.ubicacion))
+        
+        #sirvef para indicar el nombre de este "atributo"
+        ubicacion_tag.short_description= "imagen de la ubicacion"
+
+        class Meta:
+                db_table= "imagenes"
+
+                verbose_name_plural="imagenes"
+
+class Categoria(models.Model):
+        nombre = models.TextField(unique=True)
+        imagen = models.OneToOneField(to=Imagen, on_delete=models.RESTRICT, db_column="imagen_id")
+
+        class Meta:
+                db_table = "categorias"
+
+class Producto (models.Model):
+        nombre = models.TextField()
+        fechaVencimiento= models.DateField(db_column="fecha_vencimiento")
+        lote = models.TextField(null=False)
+        precio = models.FloatField(null=False)
+        categoria = models.ForeignKey(to= Categoria, on_delete=models.CASCADE, db_column="categoria_id", related_name="productos")
+        imagen = models.OneToOneField(to = Imagen, on_delete=models.RESTRICT, db_column="imagen_id", related_name="producto")
+
+        class Meta:
+                db_table= "productos"
+                #si queremos que vaya de manera ASC no es necesario hacer nada
+                # si queremos que vaya de manera DESC se le coloca el '-' al comienzo
+                # primero ordenara los productos alfabeticamente(A-Z)n y luego de manera DESC las fechas vencimiento
+                ordering = ["nombre","-fechaVencimiento"]
+
+class ManejoUsuario(BaseUserManager):
+        #para poder utilizar los metodos para obterner la data y utilizar el modelo pero para modigficar cierta funcionabilidad
+        def create_superuser(self, nombre, apellido, email, password, tipo):
+                # metodo que se manda a llamar cuando en la terminal creams un super ususario
+                if not email:
+                        raise ValueError("El correo es obligatorio")
+                #quita caracteres especiales como tildes , mayusculas y otros para que el correo n se guarde de esa manera
+                emailNormalizado = self.normalize_email(email)
+                nuevoUsuario = self.model(email = email, nombre=nombre, tipo=tipo, apellido=apellido)
+                # generar el hash de la contraseña
+                # set_password > metodo propio del auth_user que sirve para generar el hash de la contraseña usando los mimos principios que la libreria bcrypt
+                nuevoUsuario.set_password(password)
+
+                nuevoUsuario.save()
+
+
+
+class Usuario(AbstractBaseUser,PermissionsMixin):
+        nombre = models.TextField(null=False)
+        apellido = models.TextField(null=False)
+        email = models.EmailField(unique=True, null=False)
+        password= models.TextField(null=False)
+        tipo = models.TextField(choices=[("ADMIN","ADMIN"),("CAJERO","CAJERO")])  #("ADMIN","ADMIN") = ("SEMUESTRA FRONT", "SE MUESTRA EN BACK")
+        is_staff= models.BooleanField(default=False)
+
+        # Sirve para indicarle que campo utilizara para encontrar al usuario por el panel administrativo
+        USERNAME_FIELD= "email"
+
+        #campos que son requeridos al usar el comando para crear un superusuario en la consola, es indiferente si le colocamos null=False
+        REQUIRED_FIELDS= ["apellido","nombre","tipo"]
+        
+        objects =ManejoUsuario()
+
+        class Meta:
+                db_table ="usuarios"
+
